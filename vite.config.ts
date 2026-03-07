f5c86008485f36ab7297c3b38d0afdd44ac09256
groww1yt-eng <groww1yt@gmail.com>
2026-03-07 18:16:07 +0530

    Fix Vite base URL to neurolex.github.io

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/neurolex.github.io/",
   server: {
     // Listen on all network interfaces (::)
     host: "::",
